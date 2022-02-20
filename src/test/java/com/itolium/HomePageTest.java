96470cfd569b6e7eaba2fc2d110ab90d1d50607b
Avraham Granov <Avraham.granow@gmail.com>
2022-02-20 18:21:00 +0100

    rename the OpenNomeTestPage class to HometPageTest . add testing button methods on the HomePageTest

---
+++ src/test/java/com/itolium/HomePageTest.java
@@ -0,0 +1,41 @@
+package com.itolium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+
+public class HomePageTest extends TestBase {
+
+
+    public HomePageTest() {
+    }
+
+    @Test
+    public void openPageTest() {
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.className("home-title")));
+    }
+
+    @Test
+    public void buttonKarriereTest() {
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@href='/karriere']"))).click();
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//h1[contains(text(),'Karriere Page')]")));
+    }
+
+    @Test
+    public void buttonKontaktTest() {
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@href='/kontakt']"))).click();
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//h1[contains(text(),'Kontakt')]")));
+    }
+
+    @Test
+    public void buttonDatenschutzTest() {
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@href='/datenschutz']"))).click();
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.className("datenschutz-headline")));
+    }
+
+    @Test
+    public void buttonImpressumTest() {
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@href='/impressum']"))).click();
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.className("impressum-headline")));
+    }
+}
--- src/test/java/com/itolium/TestBase.java
+++ src/test/java/com/itolium/TestBase.java
@@ -1,40 +1,30 @@
 package com.itolium;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class OpenHomePageTest{
+public class TestBase {
 
-    WebDriver wd;
+
+    static WebDriver wd;
+    static WebDriverWait webDriverWait;
 
     @BeforeMethod
-    public void setUp() {
+    public void setUp() throws InterruptedException {
         wd = new ChromeDriver();
-        WebDriverWait webDriverWait = new WebDriverWait(wd, Duration.ofSeconds(10));
+        webDriverWait = new WebDriverWait(wd, Duration.ofSeconds(10));
         wd.manage().window().maximize();
         wd.navigate().to("https://itolium-app-1337.web.app/");
+
     }
 
     @AfterMethod(enabled = false)
     public void tearDown() {
         wd.quit();
     }
-
-    @Test
-    public void HomePageTest() {
-        String text = wd.findElement(By.xpath("//h1[contains(text(),'Home')]")).getText();
-        Assert.assertEquals(text, "Home");
-    }
-
-
-
-
 }
